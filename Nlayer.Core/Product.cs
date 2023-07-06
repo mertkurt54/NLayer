0271a5f27108591d1cca0b83c52480b7a4999f7a
мєят kurt <mert_kurt_fb@hotmail.com>
2023-07-06 22:26:46 +0300

    commit

--- Nlayer.Core/Product.cs
+++ Nlayer.Core/Product.cs
@@ -8,11 +8,6 @@ namespace Nlayer.Core
 {
     public class Product:BaseEntity
     {
-        public Product(string name)
-        {
-            Name = name ?? throw new ArgumentNullException(nameof(name));
-        }
-
         public string Name { get; set; }
         public int Stock { get; set; }
         public decimal Price { get; set; }
