3170eb7fe482a79ba0f1d7cd2322b22b9fed4869
SegnaMertKurt <mert.kurt@segna.com.tr>
2023-07-07 17:38:21 +0300

    commit

--- Nlayer.Core/DTOs/ProductDto.cs
+++ Nlayer.Core/DTOs/ProductDto.cs
@@ -4,15 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Nlayer.Core
+namespace Nlayer.Core.DTOs
 {
-    public class Product:BaseEntity
+    public class ProductDto : BaseDto
     {
         public string Name { get; set; }
         public int Stock { get; set; }
         public decimal Price { get; set; }
         public int CategoryId { get; set; }
-        public Category Category { get; set; }
-        public ProductFeature ProductFeature { get; set; }
     }
 }
