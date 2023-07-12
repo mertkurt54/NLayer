631426e97fb9d89968a17bca601a6a01df17371f
мєят kurt <mert_kurt_fb@hotmail.com>
2023-07-12 23:25:07 +0300

    commit

---
+++ NLayer.Caching/ProductServiceWithCaching.cs
@@ -0,0 +1,89 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc.TagHelpers.Cache;
+using Microsoft.Extensions.Caching.Memory;
+using Nlayer.Core.DTOs;
+using Nlayer.Core.Models;
+using Nlayer.Core.Repositories;
+using Nlayer.Core.Services;
+using Nlayer.Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Caching
+{
+    public class ProductServiceWithCaching : IProductService
+    {
+        private const string CacheProductKey = "produktsCache";
+        private readonly IMapper _mapper;
+        private readonly IMemoryCache _menoryCache;
+        private readonly IProductRepository _repository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductServiceWithCaching(IUnitOfWork unitOfWork, IProductRepository repository, IMemoryCache menoryCache, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _repository = repository;
+            _menoryCache = menoryCache;
+            _mapper = mapper;
+
+            if (!_menoryCache.TryGetValue(CacheProductKey, out _)) ;
+            {
+                _menoryCache.Set(CacheProductKey, _repository.GetAll().ToList());
+            }
+        }
+
+        public Task<Product> AddAsync(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> AddRangeAsync(IEnumerable<Product> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AnyAsync(Expression<Func<Product, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetByIdAsnc(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CustomResponseDto<List<ProductWithCategoryDto>>> GetPorductWithCategory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAsync(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveRangeAsync(IEnumerable<Product> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Product> Where(Expression<Func<Product, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
